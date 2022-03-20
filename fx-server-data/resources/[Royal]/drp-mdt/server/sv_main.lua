30592d81f8c0126d0869d916e7bacba28ce6a405
Podori <fpsabby@outlook.com>
2022-03-19 20:18:50 -0600

    RPC.register

--- fx-server-data/resources/[Royal]/drp-mdt/server/sv_main.lua
+++ fx-server-data/resources/[Royal]/drp-mdt/server/sv_main.lua
@@ -526,7 +526,7 @@ LoadRoyalVersion = function()
             
             CreateStuffLog("EditIncident", time, GetCharData(src).first_name..' '..GetCharData(src).last_name)
         else
-            SQL('INSERT INTO mdt_incidents (title, information, tags, officers, civilians, evidence, associated, time, author) VALUES (@title, @information, @tags, @officers, @civilians, @evidence, @associated, @time, @author)', {
+            exports.ghmattimysql:execute('INSERT INTO mdt_incidents (title, information, tags, officers, civilians, evidence, associated, time, author) VALUES (@title, @information, @tags, @officers, @civilians, @evidence, @associated, @time, @author)', {
                 ['@title'] = title, 
                 ['@information'] = information, 
                 ['@tags'] = json.encode(tags),
