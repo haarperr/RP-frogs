f49fc21122eb5b24b5f275c90dec12f2e9426015
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-21 06:23:46 +0100

    pog?

--- fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
+++ fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
@@ -1,16 +1,10 @@
-AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-      return
-    end
-    print('The resource ' .. resourceName .. ' has been started.')
+Citizen.CreateThread(function()
     while not exports or not exports['hypnonema'] do Wait(1) end
-    exports['hypnonema']:play("Hypnonema Example Screen", "https://twitch.tv/xqcow")
-    exports['hypnonema']:play("PD Monitor", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReULWfKHC4vFgFZ37Ew2O7Pej3vILuMaofZQ&usqp=CAU")
-
-    while true do
-        exports['hypnonema']:play("Hypnonema Example Screen", "https://twitch.tv/xqcow")
-        Citizen.Wait(5500)
+    local URLTOPLAY = 'https://twitch.tv/xqcow'
+    local screens = json.decode(exports.hypnonema:getScreenList())
+    for k, v in pairs(screens) do
+        if v.Name ~= nil then
+            exports.hypnonema:play(v.Name, URLTOPLAY)
+        end
     end
-
-  end)
-  
+end)
