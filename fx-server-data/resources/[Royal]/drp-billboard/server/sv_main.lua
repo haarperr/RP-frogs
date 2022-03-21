f2a227bd7cfa90fecb4d0fb5aea1744adee162bd
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-21 06:35:38 +0100

    PagMan I LOOOOOVE LUA AT 6:15AM

--- fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
+++ fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
@@ -1,10 +1,12 @@
-Citizen.CreateThread(function()
-    while not exports or not exports['hypnonema'] do Wait(1) end
-    local URLTOPLAY = 'https://twitch.tv/xqcow'
-    local screens = json.decode(exports.hypnonema:getScreenList())
-    for k, v in pairs(screens) do
-        if v.Name ~= nil then
-            exports.hypnonema:play(v.Name, URLTOPLAY)
+AddEventHandler(('on%sResourceStart'):format(IsDuplicityVersion() and 'Server' or 'Client'), function(resource)
+    if resource == GetCurrentResourceName() then
+      Citizen.CreateThread(function()
+        exports.hypnonema:play("Hypnonema Example Screen", "https://twitch.tv/xqcow")
+        exports.hypnonema:play("PD Monitor", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReULWfKHC4vFgFZ37Ew2O7Pej3vILuMaofZQ&usqp=CAU")
+        while true do
+            exports.hypnonema:play("Hypnonema Example Screen", "https://twitch.tv/xqcow")
+            Citizen.Wait(5500)
         end
+      end)
     end
 end)
