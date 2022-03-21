e910b3bc5cf5da1c10bfa4eef00a73bcfae6e330
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-21 01:53:09 +0100

    I need coffee

--- fx-server-data/resources/[Royal]/drp-mdt/client/cl_main.lua
+++ fx-server-data/resources/[Royal]/drp-mdt/client/cl_main.lua
@@ -682,9 +682,8 @@ LoadRoyalVersion = function()
     end)
     RegisterNUICallback("saveIncident", function(data, cb)
         local src = source
-        local steamIdentifier = GetPlayerIdentifiers(src)[1]
         local string = data.ID, data.title, data.information, data.tags, data.officers, data.civilians, data.evidence, data.associated, data.time
-        TriggerClientEvent('DoLongHudText', string, steamIdentifier, 1)
+        TriggerClientEvent('DoLongHudText', src, string, 1)
         RPC.execute("drp-mdt:saveIncident", data.ID, data.title, data.information, data.tags, data.officers, data.civilians, data.evidence, data.associated, data.time)
     end)
     RegisterNUICallback("removeIncidentCriminal", function(data, cb)
