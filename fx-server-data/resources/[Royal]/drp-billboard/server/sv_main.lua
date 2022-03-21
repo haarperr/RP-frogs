13c4c527bb8412f2df29201a952f51d074a8b267
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-21 11:35:29 +0100

    PauseChamp

--- fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
+++ fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
@@ -2,9 +2,12 @@ AddEventHandler(('onResourceStart'):format(IsDuplicityVersion() and 'Server' or 
     if resource == GetCurrentResourceName() then
       Citizen.CreateThread(function()
         exports.hypnonema:play("Hypnonema Example Screen", "https://twitch.tv/xqcow")
+        exports["hypnonema"]:play("Hypnonema Example Screen", "https://twitch.tv/xqcow")
         exports.hypnonema:play("PD Monitor", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReULWfKHC4vFgFZ37Ew2O7Pej3vILuMaofZQ&usqp=CAU")
+        exports["hypnonema"]:play("PD Monitor", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReULWfKHC4vFgFZ37Ew2O7Pej3vILuMaofZQ&usqp=CAU")
         while true do
             exports.hypnonema:play("pdleft", "https://i.imgur.com/vwFVrRt.gif")
+            exports["hypnonema"]:play("pdleft", "https://i.imgur.com/vwFVrRt.gif")
             Citizen.Wait(5500)
         end
       end)
