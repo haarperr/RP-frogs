69dcbc150862a11a489e45429cc331357c4f251f
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-22 04:45:41 +0100

    tf?

--- fx-server-data/resources/[Royal]/drp-heists/client/main.lua
+++ fx-server-data/resources/[Royal]/drp-heists/client/main.lua
@@ -44,7 +44,8 @@ end)
 
 RegisterNetEvent('heists:buyvpn')
 AddEventHandler('heists:buyvpn', function()
-  local user = exports["drp-base"]:getModule("Player"):GetUser(source)
+  local src = source
+  local user = exports["drp-base"]:getModule("Player"):GetUser(src)
   local cash = user:getCash()
   local price = 5000
 
