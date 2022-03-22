b19b5d410638e003c71b89f8f530421e89472a84
Podori <fpsabby@outlook.com>
2022-03-22 15:16:09 -0600

    Billboards

--- fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
+++ fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
   while not exports or not exports['hypnonema'] do Wait(1) end
-  local URLTOPLAY = 'https://www.linktovideo.com/video,mp4'
+  local URLTOPLAY = 'https://www.twitch.tv/xqcow'
   local screens = json.decode(exports.hypnonema:getScreenList())
   for k, v in pairs(screens) do
       if v.Name ~= nil then
