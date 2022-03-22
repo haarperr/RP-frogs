8f92bd4ae2376dff4fa86f214f6be29fd9dc0863
Podori <fpsabby@outlook.com>
2022-03-22 15:14:04 -0600

    Billboard

--- fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
+++ fx-server-data/resources/[Royal]/drp-billboard/server/sv_main.lua
@@ -1,14 +1,10 @@
-AddEventHandler(('onResourceStart'):format(IsDuplicityVersion() and 'Server' or 'Client'), function(resource)
-    if resource == GetCurrentResourceName() then
-      Citizen.CreateThread(function()
-        while not exports or not exports['hypnonema'] do Wait(1) end
-        local URLTOPLAY = 'https://www.twitch.tv/xqcow'
-        local screens = json.decode(exports.hypnonema:getScreenList())
-        for k, v in pairs(screens) do
-            if v.Name ~= nil then
-                exports.hypnonema:play(v.Name, URLTOPLAY)
-            end
-        end
-    end)
-    end
+Citizen.CreateThread(function()
+  while not exports or not exports['hypnonema'] do Wait(1) end
+  local URLTOPLAY = 'https://www.linktovideo.com/video,mp4'
+  local screens = json.decode(exports.hypnonema:getScreenList())
+  for k, v in pairs(screens) do
+      if v.Name ~= nil then
+          exports.hypnonema:play(v.Name, URLTOPLAY)
+      end
+  end
 end)
