70f8cbcca87e2f3d9346796f9c6fd4672646f48b
Podori <fpsabby@outlook.com>
2022-03-22 04:38:36 -0600

    Remove Discord

--- fx-server-data/resources/[Royal]/drp-assets/client/cl_main.lua
+++ fx-server-data/resources/[Royal]/drp-assets/client/cl_main.lua
@@ -968,41 +968,5 @@ AddEventHandler('firework:shotburst', function()
 	box = nil
 end)
 
-Citizen.CreateThread(function()
-	while true do
-        -- This is the Application ID (Replace this with you own)
-		SetDiscordAppId(949909010386124840)
 
-        -- Here you will have to put the image name for the "large" icon.
-		SetDiscordRichPresenceAsset('logo')
-        
-        -- (11-11-2018) New Natives:
-
-        -- Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('Roleplaying Erotically')
-       
-        -- Here you will have to put the image name for the "small" icon.
-        SetDiscordRichPresenceAssetSmall('512')
-
-        -- Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('Roleplaying Erotically')
-
-        name = GetPlayerName(PlayerId())
-        id = GetPlayerServerId(PlayerId())
-
-
-        -- (26-02-2021) New Native:
-
-        --[[ 
-            Here you can add buttons that will display in your Discord Status,
-            First paramater is the button index (0 or 1), second is the title and 
-            last is the url (this has to start with "fivem://connect/" or "https://") 
-        ]]--
-        SetDiscordRichPresenceAction(0, "Join the FiveM server", "fivem://connect/95.217.202.62/localhost:30120")
-        SetDiscordRichPresenceAction(1, "Click to join the Discord", "https://discord.gg/y59qXpanTp")
-
-        -- It updates every minute just in case.
-		Citizen.Wait(60000)
-	end
-end)
 
