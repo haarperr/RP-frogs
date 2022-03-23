d1f47325b6464a0cc1866a36748faf01a30170e1
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-23 04:19:22 +0100

    oof

--- fx-server-data/resources/[Royal]/drp-drugdeliveries/server.lua
+++ fx-server-data/resources/[Royal]/drp-drugdeliveries/server.lua
@@ -164,5 +164,4 @@ AddEventHandler('oxydelivery:builddrugstore', function(oxyStoreLocation)
 	CreateObject(`V_38_C_Sink`,coordsofbuilding.x-0.62845000,coordsofbuilding.y+4.84067900,coordsofbuilding.z+1.41538000,false,false,false)
 
 	FreezeEntityPosition(coordsofbuilding,true)
-	TriggerClientEvent('oxydelivery:teleport')
 end
