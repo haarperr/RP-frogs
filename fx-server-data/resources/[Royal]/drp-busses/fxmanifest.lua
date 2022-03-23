3f01fba0ea139aa0f004178538fd8bccfe06b216
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-23 14:35:42 +0100

    Added busses

---
+++ fx-server-data/resources/[Royal]/drp-busses/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "publictransport"
+description "Public transport AI"
+author "Scorpion01"
+version "0.2"
+
+client_scripts {
+	'client/main.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'server/main.lua',
+	'config.lua'
+}
