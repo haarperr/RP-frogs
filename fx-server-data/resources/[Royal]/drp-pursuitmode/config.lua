e316cafc72ab73af867b0b7868cf692debb9508c
Podori <fpsabby@outlook.com>
2022-03-23 20:02:19 -0600

    balance

--- fx-server-data/resources/[Royal]/drp-pursuitmode/config.lua
+++ fx-server-data/resources/[Royal]/drp-pursuitmode/config.lua
@@ -5,9 +5,9 @@ Veny.PursuitCar2 = -1336796853        -- npolstang --
 Veny.PursuitCar3 = 719025956         -- EXP --
 Veny.PursuitCar4 = -901056903           -- CVPI --
 
-Veny.MultiplierAPlus = 0.2 --- Fast
-Veny.MultiplierS = 0.3 --- Pretty Fast
-Veny.MultiplierSPlus = 0.4 --- Fastest
-Veny.MultiplierA = 0.1 --- Slowest
+Veny.MultiplierAPlus = 0.025 --- Fast
+Veny.MultiplierS = 0.050 --- Pretty Fast
+Veny.MultiplierSPlus = 0.065 --- Fastest
+Veny.MultiplierA = 0.0 --- Default
 
 Veny.notify = "DoLongHudText" --- can be mythicnotify, notification(must use nopixel notification), qbcorenotify, esxshownotification ---
