e2dde35f3b750fb7499ff4b0f220394d9efb4875
Podori <fpsabby@outlook.com>
2022-03-25 00:52:19 -0600

    Error Log?

--- fx-server-data/resources/[Royal]/drp-errorlog/server/sv_errorlog.lua
+++ fx-server-data/resources/[Royal]/drp-errorlog/server/sv_errorlog.lua
@@ -1,20 +1,23 @@
 RegisterServerEvent('error')
 AddEventHandler('error',function(resource, args)
+
     sendToDiscord("```Error in "..resource..'```', args)
 end)
 
--- function sendToDiscord(name, args, color)
---     local connect = {
---           {
---               ["color"] = 16711680,
---               ["title"] = "".. name .."",
---               ["description"] = args,
---               ["footer"] = {
---                   ["text"] = "Made by Tristen L.",
---               },
---           }
---       }
---     PerformHttpRequest('https://discord.com/api/webhooks/806715468928122881/nrgfT-swymiEFAOq_D9OIBv_WrIjj3tXHekKQ3GElvgDDDCT8no4dt-dZul7wL7A1sB7', function(err, text, headers) end, 'POST', json.encode({username = "Error Log", embeds = connect, avatar_url = "https://i.imgur.com/VuKnN5P_d.webp?maxwidth=728&fidelity=grand"}), { ['Content-Type'] = 'application/json' })
--- end
+
+
+function sendToDiscord(name, args, color)
+    local connect = {
+          {
+              ["color"] = 16711680,
+              ["title"] = "".. name .."",
+              ["description"] = args,
+              ["footer"] = {
+                  ["text"] = "Error Logger - Made By Vopori",
+              },
+          }
+      }
+    PerformHttpRequest('https://discord.com/api/webhooks/956807237010067506/RbZsiM3_qjmXIDN2BJdAnvPdY3xzWEHEtdr6vx4GzgIClMRMox3yL59Ao9eyZT6d21iW"}), { ['Content-Type'] = 'application/json' })
+end
 
 -- it must be saving into a file with io.open("test.lua", "r")
