2c344ebbf3b80e0baadacc66abad4d202ef61ab9
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-26 07:50:21 +0100

    pog

--- fx-server-data/resources/[Royal]/drp-pursuitmode/config.lua
+++ fx-server-data/resources/[Royal]/drp-pursuitmode/config.lua
@@ -2,8 +2,10 @@ Veny = {}
 
 Veny.PursuitCar1 = -1109563416           -- npolvette --
 Veny.PursuitCar2 = -1336796853        -- npolstang --
+Veny.PursuitCar2 = -1336796853        -- npolchal --
 Veny.PursuitCar3 = 719025956         -- EXP --
-Veny.PursuitCar4 = -901056903           -- CVPI --
+Veny.PursuitCar4 = -826092757           -- npolmm -- -- Nerdge actually its PursuitBike 
+Veny.PursuitCar5 = 1949729657           -- npolchall --
 
 Veny.MultiplierAPlus = 0.025 --- Fast
 Veny.MultiplierS = 0.050 --- Pretty Fast
