ca9df69f0dea0d820e764261377f2168cf5547cf
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-26 08:02:29 +0100

    oh i deleted cvpi LMAOOOOOO and smol pd buff

--- fx-server-data/resources/[Royal]/drp-pursuitmode/config.lua
+++ fx-server-data/resources/[Royal]/drp-pursuitmode/config.lua
@@ -6,10 +6,11 @@ Veny.PursuitCar2 = -1336796853        -- npolchal --
 Veny.PursuitCar3 = 719025956         -- EXP --
 Veny.PursuitCar4 = -826092757           -- npolmm -- -- Nerdge actually its PursuitBike 
 Veny.PursuitCar5 = 1949729657           -- npolchall --
+Veny.PursuitCar6 = -901056903           -- CVPI --
 
-Veny.MultiplierAPlus = 0.025 --- Fast
-Veny.MultiplierS = 0.050 --- Pretty Fast
-Veny.MultiplierSPlus = 0.065 --- Fastest
+Veny.MultiplierAPlus = 0.03 --- Fast
+Veny.MultiplierS = 0.055 --- Pretty Fast
+Veny.MultiplierSPlus = 0.07 --- Fastest
 Veny.MultiplierA = 0.0 --- Default
 
 Veny.notify = "DoLongHudText" --- can be mythicnotify, notification(must use nopixel notification), qbcorenotify, esxshownotification ---
