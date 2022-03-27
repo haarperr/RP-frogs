15509782e43d21f4d9ac86771b77e76991492c04
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-27 15:11:10 +0200

    hotfix fix

--- fx-server-data/resources/[Royal]/drp-menu/config.lua
+++ fx-server-data/resources/[Royal]/drp-menu/config.lua
@@ -218,7 +218,7 @@ rootMenuConfig =  {
         icon = "#impound-vehicle",
         enableMenu = function()
             if not exports['drp-death']:GetDeathStatus() and exports["drp-vehicles"]:NearVehicle("Distance") and not IsPedInAnyVehicle(PlayerPedId()) then
-                return (not exports["isPed"]:isPed("myJob") == 'police' or not exports["isPed"]:isPed("myJob") == 'sheriff' or not exports["isPed"]:isPed("myJob") != 'towunion' or exports["isPed"]:isPed("myJob") != 'state' or not exports["isPed"]:isPed("myJob") != 'ems')
+                if not exports["isPed"]:isPed("myJob") == 'police' or not exports["isPed"]:isPed("myJob") == 'sheriff' or not exports["isPed"]:isPed("myJob") != 'towunion' or not exports["isPed"]:isPed("myJob") != 'state' or not exports["isPed"]:isPed("myJob") != 'ems' then
             end
         end,
         subMenus = { "police:impoundsc" }
