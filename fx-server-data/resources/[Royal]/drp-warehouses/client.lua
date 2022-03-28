a19e63ac1393aef355d35be9aeae131e5dcae5a2
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-28 05:53:37 +0200

    x

--- fx-server-data/resources/[Royal]/drp-warehouses/client.lua
+++ fx-server-data/resources/[Royal]/drp-warehouses/client.lua
@@ -72,10 +72,10 @@ Citizen.CreateThread(function()
                 end
             end
             
-            local exitDistance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), warehouses[i]["x"]-9, warehouses[i]["y"], warehouses[i]["z"]-31, true)
-            if exitDistance <= 7.5 then
-                Draw3DText(warehouses[i]["x"]-9,warehouses[i]["y"],warehouses[i]["z"]-31, "Press [E] to exit")
-                if IsControlJustPressed(0, 38) and exitDistance <= 3 then
+            local exitDistance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), warehouses[i]["x"]-9, warehouses[i]["y"]+1, warehouses[i]["z"]-31, true)
+            if exitDistance <= 15 then
+                Draw3DText(warehouses[i]["x"]-9,warehouses[i]["y"]+1,warehouses[i]["z"]-31, "Press [E] to exit")
+                if IsControlJustPressed(0, 38) and exitDistance <= 1.75 then
                     -- exit the warehouse
                     DoScreenFadeOut(1)
                     SetEntityCoords(PlayerPedId(), warehouses[i]["x"], warehouses[i]["y"], warehouses[i]["z"])
