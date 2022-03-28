98afee1bcce27a0e571e64f7b2d04799d009e7b0
Podori <fpsabby@outlook.com>
2022-03-28 01:06:19 -0600

    2am lua time

--- fx-server-data/resources/[Royal]/drp-jobs/server_jobs/server.lua
+++ fx-server-data/resources/[Royal]/drp-jobs/server_jobs/server.lua
@@ -326,7 +326,7 @@ local source = source
         TriggerClientEvent('player:receiveItem', source, "oxy", math.random(2,4))
     end
     if roll == 11 then -- 5% chance 
-        if math.random(1,100) >= 95
+        if math.random(1,100) >= 95 then
         TriggerClientEvent('DoShortHudText',src, 'You see something shining in the water',2)
         TriggerClientEvent('player:receiveItem', source, "heistusb3", 1)
         else
