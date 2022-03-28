3140948bde0462f9848d487a97107b6092990081
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-28 17:36:57 +0200

    OH MY FUCKING FOD

--- fx-server-data/resources/[Royal]/drp-warehouses/server.lua
+++ fx-server-data/resources/[Royal]/drp-warehouses/server.lua
@@ -6,7 +6,7 @@ AddEventHandler("houses:confirmwarehouse", function(warehouses)
     for i, warehouse in pairs(warehouses) do  
         exports.ghmattimysql:execute("SELECT * FROM `houses2` WHERE keyname = ?", {warehouses[i]["keyName"]}, function(data)
             if not data[1] then
-                TriggerClientEvent("houses:finishuywarehouse", True, warehouses[i]["keyName"]) 
+                TriggerClientEvent("houses:finishuywarehouse", src, True, warehouses[i]["keyName"]) 
                 exports.ghmattimysql:execute('INSERT INTO `houses2` (cid, keyname, Name) VALUES (@cid, @keyname, @Name)',{
                     ['@cid'] = characterId,
                     ['@keyname'] = warehouses[i]["keyName"],
