17f4ab748bd186fa88a33fd9d1a6b67dc981abb0
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-28 05:20:17 +0200

    x

--- fx-server-data/resources/[Royal]/drp-warehouses/client.lua
+++ fx-server-data/resources/[Royal]/drp-warehouses/client.lua
@@ -32,7 +32,7 @@ function buildWarehouse(warehouseId)
 	local coordsofbuilding = GetEntityCoords(building, true)
 
     local chest = CreateObject(`imp_prop_impexp_boxpile_01`,coordsofbuilding.x,coordsofbuilding.y+2.35,coordsofbuilding.z-2,false,false,false)
-    local crafting = CreateObject(`gr_prop_gr_bench_01b`,coordsofbuilding.x,coordsofbuilding.y-2.35,coordsofbuilding.z-1.6,false,false,false)
+    local crafting = CreateObject(`gr_prop_gr_bench_01b`,coordsofbuilding.x,coordsofbuilding.y-2.45,coordsofbuilding.z-1.7,false,false,false)
 
     SetEntityRotation(crafting,0.0,0.0,-180.0,2,1)
     FreezeEntityPosition(crafting, true)
