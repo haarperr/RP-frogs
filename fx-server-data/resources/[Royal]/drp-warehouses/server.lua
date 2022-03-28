848b21fe53f89434bbdfcacb5bae8c1c5651d8cd
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-28 17:34:39 +0200

    d

--- fx-server-data/resources/[Royal]/drp-warehouses/server.lua
+++ fx-server-data/resources/[Royal]/drp-warehouses/server.lua
@@ -3,21 +3,20 @@ AddEventHandler("houses:confirmwarehouse", function(warehouses)
     src = source
     local user = exports["drp-base"]:getModule("Player"):GetUser(src)
     local characterId = user:getCurrentCharacter().id
-    local done = false
     for i, warehouse in pairs(warehouses) do  
         exports.ghmattimysql:execute("SELECT * FROM `houses2` WHERE keyname = ?", {warehouses[i]["keyName"]}, function(data)
-            if not data[1] and done == false then
-                done = true
+            if not data[1] then
                 TriggerClientEvent("houses:finishuywarehouse", True, warehouses[i]["keyName"]) 
                 exports.ghmattimysql:execute('INSERT INTO `houses2` (cid, keyname, Name) VALUES (@cid, @keyname, @Name)',{
                     ['@cid'] = characterId,
                     ['@keyname'] = warehouses[i]["keyName"],
                     ['@Name'] = warehouses[i]["locationName"],
                 })
+                return
             end
         end)
     end
     if done == false then
-        TriggerClientEvent("houses:finishuywarehouse", False, warehouses[i]["keyName"])
+        TriggerClientEvent("houses:finishuywarehouse", False, "")
     end
 end)
