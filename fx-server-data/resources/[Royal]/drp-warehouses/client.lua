5bc7c24ad711e0d65471d0f2bdb1b19499d8439b
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-28 03:34:13 +0200

    ohj pause

--- fx-server-data/resources/[Royal]/drp-warehouses/client.lua
+++ fx-server-data/resources/[Royal]/drp-warehouses/client.lua
@@ -3,7 +3,7 @@ warehouses = {
 }
 
 
-function DrawText3Ds(x,y,z, text)
+function Draw3DText(x,y,z, text)
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
     local px,py,pz=table.unpack(GetGameplayCamCoords())
     SetTextScale(0.35, 0.35)
@@ -13,8 +13,8 @@ function DrawText3Ds(x,y,z, text)
     SetTextEntry("STRING")
     SetTextCentre(1)
     AddTextComponentString(text)
-    DrawText(_x,_y)
-    local factor = 0.055
+    DrawText(_x,_y) 
+    local factor = (string.len(text)) / 370
     DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
 end
 
