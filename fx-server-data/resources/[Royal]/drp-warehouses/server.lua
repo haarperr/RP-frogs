79ce5ec90f4de9f72a0bcd99a45469ec5195203d
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-28 18:25:47 +0200

    gdcfvxdfs

--- fx-server-data/resources/[Royal]/drp-warehouses/server.lua
+++ fx-server-data/resources/[Royal]/drp-warehouses/server.lua
@@ -1,9 +1,10 @@
+local x = false
+
 RegisterNetEvent("houses:confirmwarehouse")
 AddEventHandler("houses:confirmwarehouse", function(warehouses)
     src = source
     local user = exports["drp-base"]:getModule("Player"):GetUser(src)
     local characterId = user:getCurrentCharacter().id
-    local x = false
     for i, warehouse in pairs(warehouses) do  
         exports.ghmattimysql:execute("SELECT * FROM `houses2` WHERE keyname = ?", {warehouses[i]["keyName"]}, function(data)
             if not data[1] and x == false then
