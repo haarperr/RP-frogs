2256e223084a442cc0f26dc84032594f9be86571
Podori <fpsabby@outlook.com>
2022-03-27 23:13:20 -0600

    inv

--- fx-server-data/resources/[Royal]/drp-inventory/client.js
+++ fx-server-data/resources/[Royal]/drp-inventory/client.js
@@ -212,7 +212,7 @@ on('player:receiveItem', async(id, amount, generateInformation, itemdata, return
         emit('DoLongHudText', id + ' fell on the ground because you are overweight', 2);
         let droppedItem = { slot: 3, itemid: id, amount: amount, generateInformation: generateInformation };
         cid = exports.isPed.isPed("cid");
-        emitNet('server-inventory-open', GetEntityCoords(PlayerPedId()), cid, '3', "Drop-Overweight", { "items": [droppedItem] });
+        emitNet('server-inventory-open', GetEntityCoords(PlayerPedId()), cid, '13', "Drop-Overweight", { "items": [droppedItem] });
         return;
     }
     SendNuiMessage(
@@ -590,7 +590,7 @@ function GroundInventoryScan() {
     if (row) {
         emitNet('server-inventory-open', GetEntityCoords(PlayerPedId()), cid, '1', row.name);
     } else {
-        emitNet('server-inventory-open', GetEntityCoords(PlayerPedId()), cid, '13', 'create');
+        emitNet('server-inventory-open', GetEntityCoords(PlayerPedId()), cid, '3', 'create');
     }
 }
 
