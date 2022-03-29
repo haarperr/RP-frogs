07927515389eca28de734ac320674cc30483d235
Podori <fpsabby@outlook.com>
2022-03-28 23:57:52 -0600

    huh

--- fx-server-data/resources/[Royal]/drp-drugs/client/cl_meth.lua
+++ fx-server-data/resources/[Royal]/drp-drugs/client/cl_meth.lua
@@ -101,21 +101,19 @@ end)
 
 
 function Draw3DText(x,y,z, text)
-    local factor = string_len(text) * inv_factor
-    local onScreen,_x,_y = _in(0x34E82F05DF2974F5, x, y, z, _f, _f, _r) -- GetScreenCoordFromWorldCoord
-
-    if onScreen then
-        _in(0x07C837F9A01C34C9, 0.35, 0.35) -- SetTextScale
-        _in(0x66E0276CC5F6B9DA, 4) -- SetTextFont
-        _in(0x038C1F517D7FDCF8, 1) -- SetTextProportional
-        _in(0xBE6B23FFA53FB442, 255, 255, 255, 215) -- SetTextColour
-        _in(0x25FBB336DF1804CB, "STRING") -- SetTextEntry
-        _in(0xC02F4DBFB51D988B, 1) -- SetTextCentre
-        _in(0x6C188BE134E074AA, text) -- AddTextComponentString, assumes "text" is of type string
-        _in(0xCD015E5BB0D96A57, _x, _y) -- DrawText
-        _in(0x3A618A217E5154F0, _x,_y+0.0125, 0.015 + factor, 0.03, 41, 11, 41, 68) -- DrawRect
-    end
-end
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y) 
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+end 
 
 
 
