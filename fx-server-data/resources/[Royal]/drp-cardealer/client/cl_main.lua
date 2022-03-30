f5aa27391f1407bfb410d7b5b68c1fb98ba75997
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-30 06:26:05 +0200

    PDM NERF NERF

--- fx-server-data/resources/[Royal]/drp-cardealer/client/cl_main.lua
+++ fx-server-data/resources/[Royal]/drp-cardealer/client/cl_main.lua
@@ -343,7 +343,7 @@ function setCategory(type)
     local categoryVehs = {}
     for k, v in pairs(vehicles) do
         if v.category == type then
-            table.insert(categoryVehs, {displayName = v.name, sCode = v.sCode, price = (v.price * 2)})
+            table.insert(categoryVehs, {displayName = v.name, sCode = v.sCode, price = (v.price * 2.5)})
         end
     end
     SendNUIMessage({
