7b57c2c364075613d2bb00e75122d262e2584fc0
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-03-31 19:50:57 +0200

    x

--- fx-server-data/resources/[Royal]/drp-localems/client/cl_main.lua
+++ fx-server-data/resources/[Royal]/drp-localems/client/cl_main.lua
@@ -9,8 +9,25 @@ RegisterCommand('localems', function()
         Citizen.Wait(0)
     end
 
+
+    
     local vehicle = CreateVehicle(car, coords.x+150, coords.y+150, coords.z, 0.0, true, false)
     PlaceObjectOnGroundProperly(vehicle)
+    PlaceObjectOnGroundProperly2(vehicle)
+    
+    cords = GetEntityCoords(vehicle)
+
+    local heading, vector = GetNthClosestVehicleNode(cords.x, cords.y, cords.z, 1, 0, 0)
+    local sX, sY, sZ = table.unpack(vector)
+
+    SetEntityHeading(vehicle, heading)
+    SetEntityCoords(vehicle, sX, sY, sZ)
+    
+    
+    SetVehicleDoorsLocked(vehicle, 4)
+    SetVehicleForwardSpeed(vehicle, 120.0)
+    SetVehicleEngineOn(vehicle, true, true)
+    SetVehicleLights(vehicle, 2)
     SetEntityAsMissionEntity(vehicle, true, true)
     SetVehicleHasBeenOwnedByPlayer(vehicle, true)
     local id = NetworkGetNetworkIdFromEntity(vehicle)
