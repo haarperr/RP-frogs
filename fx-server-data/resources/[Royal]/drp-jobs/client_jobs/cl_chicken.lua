07e68a4c2407b34d2de87e292d0740efca5c6738
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-02 07:59:50 +0200

    l + ratio

--- fx-server-data/resources/[Royal]/drp-jobs/client_jobs/cl_chicken.lua
+++ fx-server-data/resources/[Royal]/drp-jobs/client_jobs/cl_chicken.lua
@@ -387,7 +387,7 @@ AddEventHandler("drp-chickens:sell", function()
 	LoadDict('amb@medic@standing@tendtodead@exit')
 	TaskPlayAnim(GetPlayerPed(-1), 'amb@medic@standing@tendtodead@exit', 'exit', 8.0, -8.0, -1, 1, 0.0, 0, 0, 0)
 	if (finished == 100) then		
-		if exports["drp-inventory"]:hasEnoughOfItem("lqprotein",toSell,false) and GetDistanceBetweenCoords(x, y, z, GetEntityCoords(GetPlayerPed(-1)).x, GetEntityCoords(GetPlayerPed(-1)).y, GetEntityCoords(GetPlayerPed(-1)).z, true) >= 25 then
+		if exports["drp-inventory"]:hasEnoughOfItem("lqprotein",toSell,false) and GetDistanceBetweenCoords(x, y, z, GetEntityCoords(GetPlayerPed(-1)).x, GetEntityCoords(GetPlayerPed(-1)).y, GetEntityCoords(GetPlayerPed(-1)).z, true) <= 25 then
 			TriggerEvent('inventory:removeItem', 'lqprotein', toSell)
 			TriggerServerEvent('chickenpayment:pay', math.random(115, 165) * toSell)
 			ClearPedTasksImmediately(PlayerPedId())
