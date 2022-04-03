43b671bed5b551e23ebe28d0cefe14747994f181
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-03 22:14:48 +0200

    xqc

--- fx-server-data/resources/[Royal]/drp-drone/cl_drone.lua
+++ fx-server-data/resources/[Royal]/drp-drone/cl_drone.lua
@@ -4,16 +4,6 @@ AddEventHandler("drp-drone:spawnDrone", function()
     if exports['drp-inventory']:hasEnoughOfItem('drone_lspd', 1) then
         local currentPed = GetPlayerPed(-1)
 
-        -- Load the other ped
-        RequestModel(GetHashKey("s_m_y_cop_01"))
-        while not HasModelLoaded(GetHashKey("s_m_y_cop_01")) do
-            Wait(1)
-        end
-
-        -- spawn player clone with same clothes
-        local playerClone = CreatePed(4, GetHashKey("s_m_y_cop_01"), GetEntityCoords(currentPed), GetEntityHeading(currentPed), true, true)
-        ClonePedToTarget(currentPed, playerClone)
-        
         -- load drone
         RequestModel(GetHashKey("drone_lspd"))
         while not HasModelLoaded(GetHashKey("drone_lspd")) do
@@ -25,7 +15,7 @@ AddEventHandler("drp-drone:spawnDrone", function()
         local drone = CreateVehicle(GetHashKey("drone_lspd"), 481.6537, -982.0282, 41.0068, 182.5093, true, true)
         
         SetVehicleOnGroundProperly(drone)
-        SetPedIntoVehicle(playerClone, drone, -1)
+        SetPedIntoVehicle(currentPed, drone, -1)
         
         TriggerEvent("DoLongHudText","Have fun in your Drone!", 1)        
     else
