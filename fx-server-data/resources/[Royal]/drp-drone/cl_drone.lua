7b7df75410fd71201a141039a92611de55da32c2
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-03 22:16:34 +0200

    x

--- fx-server-data/resources/[Royal]/drp-drone/cl_drone.lua
+++ fx-server-data/resources/[Royal]/drp-drone/cl_drone.lua
@@ -2,17 +2,17 @@ RegisterNetEvent("drp-drone:spawnDrone")
 AddEventHandler("drp-drone:spawnDrone", function()
     if exports['drp-inventory']:hasEnoughOfItem('drone_lspd', 1) then
         local currentPed = GetPlayerPed(-1)
-
+        
+        TriggerEvent("DoLongHudText","Loading", 2)   
         -- load drone
         RequestModel(GetHashKey("drone_lspd"))
         while not HasModelLoaded(GetHashKey("drone_lspd")) do
             Wait(1)
         end
-
+        TriggerEvent("DoLongHudText","Loaded", 2)   
         -- spawn drone
         local drone = CreateVehicle("drone_lspd", 481.6537, -982.0282, 41.0068, 182.5093, true, true)
-        
-        SetVehicleOnGroundProperly(drone)
+        TriggerEvent("DoLongHudText","Spawned", 2)   
         SetPedIntoVehicle(currentPed, drone, -1)
         
         TriggerEvent("DoLongHudText","Have fun in your Drone!", 1)        
