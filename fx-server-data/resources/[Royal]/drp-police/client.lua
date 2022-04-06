19bd634bdc8b4835fb21bca12aaf4fe7b292c7e4
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-06 13:24:31 +0200

    x

--- fx-server-data/resources/[Royal]/drp-police/client.lua
+++ fx-server-data/resources/[Royal]/drp-police/client.lua
@@ -616,6 +616,8 @@ AddEventHandler('police:getArrested2', function(cuffer)
 				TriggerEvent('DoLongHudText', 'You slipped out of cuffs !',1)
 				TriggerEvent("handcuffed",false)
 				return end
+			end
+		end
 	end
 	
 	if #(GetEntityCoords( PlayerPedId()) - GetEntityCoords(cuffPed)) < 2.5 and finished ~= 100 then
