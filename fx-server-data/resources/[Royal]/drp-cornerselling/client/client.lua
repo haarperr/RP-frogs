ea59a94bb5ef99526b0fbbb6f4cb542e8f5688d2
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-07 04:29:45 +0200

    x

--- fx-server-data/resources/[Royal]/drp-cornerselling/client/client.lua
+++ fx-server-data/resources/[Royal]/drp-cornerselling/client/client.lua
@@ -91,61 +91,53 @@ function HasItems(amount)
 end
 
 function sell_items()
-    Citizen.Trace("Selling\n")
-    local chance = math.random(1, 3)
-    Citizen.Trace(chance .. "\n")
-    local hasitems, amount = HasItems(chance)
-     
 
-    if hasitems then
-        Citizen.Trace("Has items\n")
-        Citizen.Trace(amount .. "\n")
-        local plycoords = GetEntityCoords(PlayerPedId())
-        TaskTurnPedToFaceCoord(ped, plycoords.x, plycoords.y, plycoords.z, 1250)
-        Citizen.Wait(1250)
-        SetEntityAsMissionEntity(ped)
-        TaskStandStill(ped, 9.0)
-        local finished = exports['drp-taskbar']:taskBar(5000, "Atempting Sale")
-        local plycoords = GetEntityCoords(PlayerPedId())
-        recent_ped = ped
-        if (finished == 100) then
-            if math.random(1, 100) >= 40 then 
-                giveAnim(ped)
-                giveAnim(PlayerPedId())
-                Citizen.Wait(1500)
-                local sellableItems = {
-                    [1] = {name = 'oxy', amount = math.random(1,3), price=math.random(80,120)},
-                    [2] = {name = 'methlabproduct', amount = math.random(1,2), price=math.random(450, 575)},
-                    [3] = {name = 'weedq', amount = math.random(1,3), price=420}, 
-                }
+    local plycoords = GetEntityCoords(PlayerPedId())
+    TaskTurnPedToFaceCoord(ped, plycoords.x, plycoords.y, plycoords.z, 1250)
+    Citizen.Wait(1250)
+    SetEntityAsMissionEntity(ped)
+    TaskStandStill(ped, 9.0)
+    local finished = exports['drp-taskbar']:taskBar(5000, "Atempting Sale")
+    local plycoords = GetEntityCoords(PlayerPedId())
+    recent_ped = ped
+    if (finished == 100) then
+        if math.random(1, 100) >= 40 then 
+            giveAnim(ped)
+            giveAnim(PlayerPedId())
+            Citizen.Wait(1500)
+            local sellableItems = {
+                [1] = {name = 'oxy', amount = math.random(1,3), price=math.random(80,120)},
+                [2] = {name = 'methlabproduct', amount = math.random(1,2), price=math.random(450, 575)},
+                [3] = {name = 'weedq', amount = math.random(1,3), price=420}, 
+            }
 
-                local pog = false
-        
-                -- randomize the sellable items
-                for i = 1, #sellableItems do
-                    local randomIndex = math.random(#sellableItems)
-                    sellableItems[i] = sellableItems[randomIndex]
-                    
-                    if exports["drp-inventory"]:hasEnoughOfItem(sellableItems[i].name,sellableItems[i].amount,false) then
-                        TriggerEvent("inventory:removeItem",sellableItems[i].name,sellableItems[i].amount)
-                        TriggerServerEvent('mission:completed', sellableItems[i].price * sellableItems[i].amount)
-                        pog = true
-                        break
-                    else
-                        TriggerEvent("DoLongHudText", "You dont got what I want!", 2)
-                    end
+            local pog = false
+    
+            -- randomize the sellable items
+            for i = 1, #sellableItems do
+                local randomIndex = math.random(#sellableItems)
+                sellableItems[i] = sellableItems[randomIndex]
+                
+                if exports["drp-inventory"]:hasEnoughOfItem(sellableItems[i].name,sellableItems[i].amount,false) then
+                    TriggerEvent("inventory:removeItem",sellableItems[i].name,sellableItems[i].amount)
+                    TriggerServerEvent('mission:completed', sellableItems[i].price * sellableItems[i].amount)
+                    pog = true
+                    break
+                else
+                    TriggerEvent("DoLongHudText", "You dont got what I want!", 2)
                 end
-                if math.random(1, 100) >= 20 then
-                    TriggerEvent("civilian:alertPolice", 15.0, "drugsale", 0)
-                end
-            else
+            end
+            if math.random(1, 100) >= 20 then
+                TriggerEvent("civilian:alertPolice", 15.0, "drugsale", 0)
+            end
+        else
+            SetPedAsNoLongerNeeded(recent_ped)
+            if math.random(1,100) >= 20 then
                 SetPedAsNoLongerNeeded(recent_ped)
-                TriggerEvent("DoLongHudText", "They are not interested anymore!", 2)
-                if math.random(1,100) >= 20 then
-                    SetPedAsNoLongerNeeded(recent_ped)
-                    TriggerEvent("DoLongHudText", "They are not interested anymore and called the cops!", 2)
-                    TriggerEvent("civilian:alertPolice", 15.0, "drugsale", 0)
-                end
+                TriggerEvent("DoLongHudText", "They are not interested and called the cops!", 2)
+                TriggerEvent("civilian:alertPolice", 15.0, "drugsale", 0)
+            else
+                TriggerEvent("DoLongHudText", "They are not interested!", 2)
             end
         end
     end
