079a9dad62e9db9dc944f145989afc7fb8584955
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-07 06:54:50 +0200

    x

--- fx-server-data/resources/[Royal]/drp-jobs/client_jobs/cl_burgershot.lua
+++ fx-server-data/resources/[Royal]/drp-jobs/client_jobs/cl_burgershot.lua
@@ -761,16 +761,17 @@ local currentMenu = nil
 
 RegisterNetEvent("bsdelivery:getJob")
 AddEventHandler("bsdelivery:getJob", function()
-    local rank = exports["isPed"]:GroupRank("burger_shot")
-    if rank <= 1 then
-        if hasJob == true then
-            TriggerEvent("DoLongHudText", "You already have a job!", 2)
-        else
-            TriggerEvent("bsdelivery:getTheJob")
-        end
-    else
-        TriggerEvent("DoLongHudText", "You dont work here", 2)
-    end
+    TriggerEvent("DoLongHudText", "Come back later.", 2)
+    -- local rank = exports["isPed"]:GroupRank("burger_shot")
+    -- if rank <= 1 then
+    --     if hasJob == true then
+    --         TriggerEvent("DoLongHudText", "You already have a job!", 2)
+    --     else
+    --         TriggerEvent("bsdelivery:getTheJob")
+    --     end
+    -- else
+    --     TriggerEvent("DoLongHudText", "You dont work here", 2)
+    -- end
 end)
 
 RegisterNetEvent("bsdelivery:getTheJob")
@@ -791,7 +792,7 @@ AddEventHandler("bsdelivery:getTheJob", function()
 
     -- get every item name
     local items = {}
-    
+
 
 
 
