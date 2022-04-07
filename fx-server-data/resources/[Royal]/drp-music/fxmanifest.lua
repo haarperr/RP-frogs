f26db593a2469af33c3ed84898d549e5030c7644
Voporii <99855926+Voporii@users.noreply.github.com>
2022-04-07 09:50:33 -0600

    music

--- fx-server-data/resources/[Royal]/drp-music/fxmanifest.lua
+++ fx-server-data/resources/[Royal]/drp-music/fxmanifest.lua
@@ -10,17 +10,17 @@ files {
 }
 
 shared_scripts {
-    "@drp-lib/shared/sh_util.lua",
+    "@drp-rpc/shared/sh_util.lua",
     "shared/*"
 }
 
 server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "@drp-lib/server/sv_rpc.lua",
+    "@mysql-async/lib/MySQL.lua",
+    "@drp-rpc/server/sv_rpc.lua",
     "server/*"
 }
 
 client_scripts {
-    "@drp-lib/client/cl_rpc.lua",
+    "@drp-rpc/client/cl_rpc.lua",
     "client/*"
 }
