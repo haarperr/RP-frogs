ece752d1714edc497e3247f8a3d57e6e04b389fd
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-07 07:01:35 +0200

    x

--- fx-server-data/resources/[Royal]/drp-cornerselling/fxmanifest.lua
+++ fx-server-data/resources/[Royal]/drp-cornerselling/fxmanifest.lua
@@ -6,8 +6,8 @@ author "Kalxie"
 description "A simple drug selling system made for IconicRP"
 version "1.0.0"
 
-server_script '@iconic-remotecalls/server/sv_main.lua'
-client_script '@iconic-remotecalls/client/cl_main.lua'
+server_script '/server/sv_main.lua'
+client_script '/client/cl_main.lua'
 
 
 client_scripts {
