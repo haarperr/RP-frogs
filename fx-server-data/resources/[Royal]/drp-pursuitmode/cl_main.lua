178974b1ee71a8ec31e6a1ecfedaaf5d62b797c6
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-10 02:31:46 +0200

    only driver fix

--- fx-server-data/resources/[Royal]/drp-pursuitmode/cl_main.lua
+++ fx-server-data/resources/[Royal]/drp-pursuitmode/cl_main.lua
@@ -27,46 +27,50 @@ CreateThread(function()
     while true do
         local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
         if IsControlJustPressed(0, 178) then --delete
-            if vehicle ~= 0 then
-                local vehiclehash = GetEntityModel(vehicle)
-                if isPursuitCar(vehiclehash) then
-                    local defaultcarspeed = GetVehicleHandlingFloat(vehicle, 'CHandlingData', 'fInitialDriveForce')
-                    if mode4 then
-                        mode1 = true
-                        mode2 = false
-                        mode3 = false
-                        mode4 = false
-                        TriggerEvent('DoLongHudText', "Pursuit Mode: A+" , 1)
-                        SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierAPlus-Veny.MultiplierA)
-                    elseif mode1 then
-                        mode1 = false
-                        mode2 = true
-                        mode3 = false
-                        mode4 = false
-                        TriggerEvent('DoLongHudText', "Pursuit Mode: S" , 1)
-                        SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierS-Veny.MultiplierAPlus)
-                    elseif mode2 then
-                        mode1 = false
-                        mode2 = false
-                        mode3 = true
-                        mode4 = false
-                        TriggerEvent('DoLongHudText', "Pursuit Mode: S+" , 1)
-                        SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierSPlus-Veny.MultiplierS)
-                    elseif mode3 then
-                        mode1 = false
-                        mode2 = false
-                        mode3 = false
-                        mode4 = true
-                        TriggerEvent('DoLongHudText', "Pursuit Mode: A" , 1)
-                        SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierA-Veny.MultiplierSPlus)
+            -- check if driver
+            if GetPedInVehicleSeat(vehicle, -1) == PlayerPedId() then
+                
+                if vehicle ~= 0 then
+                    local vehiclehash = GetEntityModel(vehicle)
+                    if isPursuitCar(vehiclehash) then
+                        local defaultcarspeed = GetVehicleHandlingFloat(vehicle, 'CHandlingData', 'fInitialDriveForce')
+                        if mode4 then
+                            mode1 = true
+                            mode2 = false
+                            mode3 = false
+                            mode4 = false
+                            TriggerEvent('DoLongHudText', "Pursuit Mode: A+" , 1)
+                            SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierAPlus-Veny.MultiplierA)
+                        elseif mode1 then
+                            mode1 = false
+                            mode2 = true
+                            mode3 = false
+                            mode4 = false
+                            TriggerEvent('DoLongHudText', "Pursuit Mode: S" , 1)
+                            SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierS-Veny.MultiplierAPlus)
+                        elseif mode2 then
+                            mode1 = false
+                            mode2 = false
+                            mode3 = true
+                            mode4 = false
+                            TriggerEvent('DoLongHudText', "Pursuit Mode: S+" , 1)
+                            SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierSPlus-Veny.MultiplierS)
+                        elseif mode3 then
+                            mode1 = false
+                            mode2 = false
+                            mode3 = false
+                            mode4 = true
+                            TriggerEvent('DoLongHudText', "Pursuit Mode: A" , 1)
+                            SetVehicleHandlingField(vehicle, "CHandlingData", "fInitialDriveForce", defaultcarspeed+Veny.MultiplierA-Veny.MultiplierSPlus)
+                        end
+                    else
+                    TriggerEvent('DoLongHudText', "You are not in a pursuit car" , 2)
                     end
                 else
-                   TriggerEvent('DoLongHudText', "You are not in a pursuit car" , 2)
+                    TriggerEvent('DoLongHudText', "You are not in a car" , 2)
                 end
-            else
-                TriggerEvent('DoLongHudText', "You are not in a car" , 2)
             end
         end
-        Wait(0)
+        Wait(1)
     end
 end)
