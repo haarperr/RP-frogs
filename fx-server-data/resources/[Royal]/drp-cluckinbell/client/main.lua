95069e19c3839f3ef7d1ac75545258808b9d949e
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-12 02:45:16 +0200

    x

--- fx-server-data/resources/[Royal]/drp-cluckinbell/client/main.lua
+++ fx-server-data/resources/[Royal]/drp-cluckinbell/client/main.lua
@@ -68,15 +68,15 @@ end)
 
 RegisterNetEvent("drp-cluckin:startMinigame")
 AddEventHandler("drp-cluckin:startMinigame", function()
-    local finished1 = exports["drp-bar"]:taskBar(1000,math.random(1,2))
+    local finished1 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
     if finished1 == 100 then 
-        local finished2 = exports["drp-bar"]:taskBar(1000,math.random(1,2))
+        local finished2 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
         if finished2 == 100 then
-            local finished3 = exports["drp-bar"]:taskBar(1000,math.random(1,2))
+            local finished3 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
             if finished3 == 100 then
-                local finished4 = exports["drp-bar"]:taskBar(1000,math.random(1,2))
+                local finished4 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
                 if finished4 == 100 then
-                    local finished5 = exports["drp-bar"]:taskBar(1000,math.random(1,2))
+                    local finished5 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
                     if finished5 == 100 then
                         if math.random(1,100) == 1 then
 						    TriggerEvent('player:receiveItem', "cbcoupongolden", math.random(1))	
