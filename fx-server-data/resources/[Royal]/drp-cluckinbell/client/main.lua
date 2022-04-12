1c1e5953c63998c34d2ea3a620582bfe7d853583
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-12 02:46:05 +0200

    x

--- fx-server-data/resources/[Royal]/drp-cluckinbell/client/main.lua
+++ fx-server-data/resources/[Royal]/drp-cluckinbell/client/main.lua
@@ -68,6 +68,9 @@ end)
 
 RegisterNetEvent("drp-cluckin:startMinigame")
 AddEventHandler("drp-cluckin:startMinigame", function()
+    if math.random(1, 100) >= 70 then
+        TriggerEvent('inventory:removeItem', "cbcontroller", 1)
+    end
     local finished1 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
     if finished1 == 100 then 
         local finished2 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
@@ -78,21 +81,31 @@ AddEventHandler("drp-cluckin:startMinigame", function()
                 if finished4 == 100 then
                     local finished5 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
                     if finished5 == 100 then
-                        if math.random(1,100) == 1 then
-						    TriggerEvent('player:receiveItem', "cbcoupongolden", math.random(1))	
-                        else
-                            TriggerEvent('player:receiveItem', "cbcoupon", math.random(1,3))	
+                        local finished6 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
+                        if finished6 == 100 then
+                            local finished7 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
+                            if finished7 == 100 then
+                                local finished8 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
+                                if finished8 == 100 then
+                                    local finished9 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
+                                    if finished9 == 100 then
+                                        local finished10 = exports["drp-bar"]:taskBar(5000,math.random(4,8))
+                                        if finished10 == 100 then
+                                            if math.random(1,100) == 1 then
+                                                TriggerEvent('player:receiveItem', "cbcoupongolden", math.random(1))	
+                                            else
+                                                TriggerEvent('player:receiveItem', "cbcoupon", math.random(1,3))	
+                                            end
+                                        end
+                                    end
+                                end
+                            end
                         end
                     end
                 end
             end
         end
-    end
-
-    if math.random(1, 100) >= 70 then
-        TriggerEvent('inventory:removeItem', "petchicken", 1)
-    end
-    
+    end   
 end)
 
 
