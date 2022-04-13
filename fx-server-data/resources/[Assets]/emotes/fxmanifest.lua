150f54aee9493c1b262e9f622bc2ef8a50694fb2
Voporii <99855926+Voporii@users.noreply.github.com>
2022-04-12 18:41:13 -0600

    d

--- fx-server-data/resources/[Assets]/emotes/fxmanifest.lua
+++ fx-server-data/resources/[Assets]/emotes/fxmanifest.lua
@@ -2,9 +2,9 @@ fx_version 'cerulean'
 games {'gta5'}
 
 client_script "@warmenu/warmenu.lua"
-client_script "@np-errorlog/client/cl_errorlog.lua"
-client_script "@np-lib/client/cl_flags.lua"
-client_script "@np-sync/client/lib.lua"
+client_script "@drp-errorlog/client/cl_errorlog.lua"
+client_script "@drp-lib/client/cl_flags.lua"
+client_script "@drp-sync/client/lib.lua"
 
 client_script "client.lua"
 client_script "cl_menu.lua"
