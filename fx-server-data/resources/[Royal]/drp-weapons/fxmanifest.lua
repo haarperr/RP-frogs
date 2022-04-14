1164b09fa2ebbd49fa1ef079802f1954d1a611f5
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 11:21:09 +0200

    x

--- fx-server-data/resources/[Royal]/drp-weapons/fxmanifest.lua
+++ fx-server-data/resources/[Royal]/drp-weapons/fxmanifest.lua
@@ -11,21 +11,11 @@ server_script 'server.lua'
 
 client_scripts {
   "@drp-errorlog/client/cl_errorlog.lua",
-  "@drp-lib/client/cl_rpc.lua",
-  "modes.lua",
-  "melee.lua",
-  "pickups.lua"
+	"@drp-lib/client/cl_rpc.lua",
+	"modes.lua",
+	"melee.lua",
+	"pickups.lua"
 }
-
-shared_script {
-  "@drp-lib/shared/sh_util.lua"
-}
-
-server_scripts {
-  "@drp-lib/server/sv_rpc.lua",
-  "@drp-lib/server/sv_sql.lua",
-}
-
 server_export 'getWeaponMetaData'
 server_export "updateWeaponMetaData"
 
