a4b00968fbc9c49e17939a3f6617b874d55a1af0
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 13:11:07 +0200

    Revert "x"
    This reverts commit cc91a5c75cb91fec26701480d8a47bf34ba12c1a.

--- fx-server-data/resources/[Royal]/drp-barriers/server.lua
+++ fx-server-data/resources/[Royal]/drp-barriers/server.lua
@@ -1,15 +1,13 @@
 RegisterNetEvent("drp-barriers:stoptraffic")
 AddEventHandler("drp-barriers:stoptraffic", function(vec3)
     -- get all peds in a radius of 10
-    while true do
-        local peds = GetAllPeds()
-        for k, v in pairs(peds) do
-            if GetEntityCoords(v) then
-                local distance = Vdist(vec3.x, vec3.y, vec3.z, GetEntityCoords(v))
-                if distance < 10.0 then
-                    if IsPedInVehicle(v, GetVehiclePedIsIn(v, false), false) then
-                        SetEntityMaxSpeed(v, 0.0)
-                    end
+    local peds = GetAllPeds()
+    for k, v in pairs(peds) do
+        if GetEntityCoords(v) then
+            local distance = Vdist(vec3.x, vec3.y, vec3.z, GetEntityCoords(v))
+            if distance < 10.0 then
+                if IsPedInVehicle(v, GetVehiclePedIsIn(v, false), false) then
+                    SetEntityMaxSpeed(v, 0.0)
                 end
             end
         end
