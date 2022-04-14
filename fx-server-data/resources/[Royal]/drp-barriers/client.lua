ad2ab7072c8edd2adebece7e8ff5237a6bf4df8b
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 12:47:21 +0200

    x

--- fx-server-data/resources/[Royal]/drp-barriers/client.lua
+++ fx-server-data/resources/[Royal]/drp-barriers/client.lua
@@ -24,6 +24,33 @@ RegisterCommand("barrier", function()
 	end
 end)
 
+RegisterCommand("pickup", function()
+	local can = (exports["isPed"]:isPed("myJob") == 'police' or exports["isPed"]:isPed("myJob") == 'sheriff' or exports["isPed"]:isPed("myJob") == 'state' or exports["isPed"]:isPed("myJob") == 'judge' or exports["isPed"]:isPed("myJob") == 'district_attorney' or exports["isPed"]:isPed("myJob") == 'public_defender') and not exports['drp-death']:GetDeathStatus()
+    if can then 
+		TriggerEvent('drp-barriers:cleanup')
+	end
+end)
+
+AddEventHandler("drp-barriers:cleanup", function()
+	-- delete every model prop_mp_cone_01 and prop_mp_barrier_02b and prop_barrier_work05 in an radius of 10
+	local cone = GetClosestObjectOfType(GetEntityCoords(PlayerPedId()), 10.0, GetHashKey("prop_mp_cone_01"), false, false, false)
+	local barrier = GetClosestObjectOfType(GetEntityCoords(PlayerPedId()), 10.0, GetHashKey("prop_mp_barrier_02b"), false, false, false)
+	local barrier2 = GetClosestObjectOfType(GetEntityCoords(PlayerPedId()), 10.0, GetHashKey("prop_barrier_work05"), false, false, false)
+
+	if DoesEntityExist(cone) then
+		DeleteEntity(cone)
+	end
+
+	if DoesEntityExist(barrier) then
+		DeleteEntity(barrier)
+	end
+
+	if DoesEntityExist(barrier2) then
+		DeleteEntity(barrier2)
+	end
+end)
+
+
 
 
 AddEventHandler('drp-barriers:cone', function()
