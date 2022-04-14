f4bb22cfeeb6f9295d704e17ff8bb7f33c06e81e
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 13:11:53 +0200

    x

--- fx-server-data/resources/[Royal]/drp-barriers/client.lua
+++ fx-server-data/resources/[Royal]/drp-barriers/client.lua
@@ -7,7 +7,6 @@ RegisterCommand("cone", function()
 	local can = (exports["isPed"]:isPed("myJob") == 'police' or exports["isPed"]:isPed("myJob") == 'sheriff' or exports["isPed"]:isPed("myJob") == 'state' or exports["isPed"]:isPed("myJob") == 'judge' or exports["isPed"]:isPed("myJob") == 'district_attorney' or exports["isPed"]:isPed("myJob") == 'public_defender') and not exports['drp-death']:GetDeathStatus()
     if can then 
 		TriggerEvent('drp-barriers:cone')
-		TriggerServerEvent("drp-barriers:stoptraffic", GetEntityCoords(GetPlayerPed(-1)))
 	end
 end)
 
@@ -15,7 +14,6 @@ RegisterCommand("sbarrier", function()
 	local can = (exports["isPed"]:isPed("myJob") == 'police' or exports["isPed"]:isPed("myJob") == 'sheriff' or exports["isPed"]:isPed("myJob") == 'state' or exports["isPed"]:isPed("myJob") == 'judge' or exports["isPed"]:isPed("myJob") == 'district_attorney' or exports["isPed"]:isPed("myJob") == 'public_defender') and not exports['drp-death']:GetDeathStatus()
     if can then 
 		TriggerEvent('drp-barriers:sbarrier')
-		TriggerServerEvent("drp-barriers:stoptraffic", GetEntityCoords(GetPlayerPed(-1))))
 	end
 end)
 
@@ -23,7 +21,6 @@ RegisterCommand("barrier", function()
 	local can = (exports["isPed"]:isPed("myJob") == 'police' or exports["isPed"]:isPed("myJob") == 'sheriff' or exports["isPed"]:isPed("myJob") == 'state' or exports["isPed"]:isPed("myJob") == 'judge' or exports["isPed"]:isPed("myJob") == 'district_attorney' or exports["isPed"]:isPed("myJob") == 'public_defender') and not exports['drp-death']:GetDeathStatus()
     if can then 
 		TriggerEvent('drp-barriers:barrier')
-		TriggerServerEvent("drp-barriers:stoptraffic", GetEntityCoords(GetPlayerPed(-1))))
 	end
 end)
 
--- fx-server-data/resources/[Royal]/drp-barriers/fxmanifest.lua
+++ fx-server-data/resources/[Royal]/drp-barriers/fxmanifest.lua
@@ -1,8 +1,6 @@
 fx_version 'cerulean'
 games {'gta5'}
 
-client_script "@np-errorlog/client/cl_errorlog.lua"
-client_script "@np-locales/client/lib.lua"
+client_script "@drp-errorlog/client/cl_errorlog.lua"
+client_script "@drp-locales/client/lib.lua"
 client_script "client.lua"
-
-server_script "server.lua"
