fa4a55b8527898267f155a7ae24199ffaee7d2f5
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 09:40:58 +0200

    x

--- fx-server-data/resources/[Royal]/drp-uniondepository/client.lua
+++ fx-server-data/resources/[Royal]/drp-uniondepository/client.lua
@@ -13,67 +13,71 @@ AddEventHandler("drp-ud:elevatorcheck", function()
     local elevator = vector3(9.9859, -667.3959, 33.4492)
     TriggerServerEvent("drp-ud:requestVariables")
     local distance = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, elevator.x, elevator.y, elevator.z, true)
-    if distance < 3 and ongoingHeist == false then
-        if exports["drp-duty"]:LawAmount() >= copCount then
-            -- heist starts
-            RequestAnimDict("anim@heists@ornate_bank@thermal_charge")
-            RequestModel("hei_p_m_bag_var22_arm_s")
-            RequestNamedPtfxAsset("scr_ornate_heist")
-            while not HasAnimDictLoaded("anim@heists@ornate_bank@thermal_charge") and not HasModelLoaded("hei_p_m_bag_var22_arm_s") and not HasNamedPtfxAssetLoaded("scr_ornate_heist") do
-            Citizen.Wait(50)
-            end
-            if not HasNamedPtfxAssetLoaded("scr_ornate_heist") then
+    if distance < 3 then
+        if ongoingHeist == false then
+            if exports["drp-duty"]:LawAmount() >= copCount then
+                -- heist starts
+                RequestAnimDict("anim@heists@ornate_bank@thermal_charge")
+                RequestModel("hei_p_m_bag_var22_arm_s")
                 RequestNamedPtfxAsset("scr_ornate_heist")
-                while not HasNamedPtfxAssetLoaded("scr_ornate_heist") do
-                    Wait(1)
+                while not HasAnimDictLoaded("anim@heists@ornate_bank@thermal_charge") and not HasModelLoaded("hei_p_m_bag_var22_arm_s") and not HasNamedPtfxAssetLoaded("scr_ornate_heist") do
+                Citizen.Wait(50)
                 end
+                if not HasNamedPtfxAssetLoaded("scr_ornate_heist") then
+                    RequestNamedPtfxAsset("scr_ornate_heist")
+                    while not HasNamedPtfxAssetLoaded("scr_ornate_heist") do
+                        Wait(1)
+                    end
+                end
+                TaskGoStraightToCoord(PlayerPedId(), 8.4783, -667.8687, 33.4497, 1.0, -1, 177.2868, 0.0)
+                Citizen.Wait(4000)
+                TriggerEvent('inventory:removeItem', 'elevatorhackingdevice', 1)
+                exports["blz-memory"]:thermiteminigame(1, 3, 3, 8,
+                function()
+                    local rotx, roty, rotz = table.unpack(vec3(GetEntityRotation(PlayerPedId())))
+                    local bagscene = NetworkCreateSynchronisedScene(8.4783, -667.8687, 33.4497, rotx, roty, rotz + 1.1, 2, false, false, 1065353216, 0, 1.3)
+                    local bag = CreateObject(GetHashKey("hei_p_m_bag_var22_arm_s"), 8.4783, -667.8687, 33.4497,  true,  true, false)
+
+                    SetEntityCollision(bag, false, true)
+                    NetworkAddPedToSynchronisedScene(ped, bagscene, "anim@heists@ornate_bank@thermal_charge", "thermal_charge", 1.2, -4.0, 1, 16, 1148846080, 0)
+                    NetworkAddEntityToSynchronisedScene(bag, bagscene, "anim@heists@ornate_bank@thermal_charge", "bag_thermal_charge", 4.0, -8.0, 1)
+                    SetPedComponentVariation(ped, 5, 0, 0, 0)
+                    NetworkStartSynchronisedScene(bagscene)
+                    Citizen.Wait(1500)
+                    local x, y, z = table.unpack(GetEntityCoords(ped))
+                    local bomba = CreateObject(GetHashKey("hei_prop_heist_thermite"), x, y, z + 0.3, true, true, true)
+
+                    SetEntityCollision(bomba, false, true)
+                    AttachEntityToEntity(bomba, ped, GetPedBoneIndex(ped, 28422), 0, 0, 0, 0, 0, 200.0, true, true, false, true, 1, true)
+                    Citizen.Wait(2000)
+                    DeleteObject(bag)
+                    SetPedComponentVariation(ped, 5, 45, 0, 0)
+                    DetachEntity(bomba, 1, 1)
+                    FreezeEntityPosition(bomba, true)
+                    TriggerServerEvent("drp-ud:particleserver2", method, vector3(x, y, z + 0.3))
+                    SetPtfxAssetNextCall("scr_ornate_heist")
+                    local effect = StartParticleFxLoopedAtCoord("scr_heist_ornate_thermal_burn", x, y, z + 0.3, 0.0, 0.0, 0.0, 1.0, false, false, false, false)
+                    
+                    NetworkStopSynchronisedScene(bagscene)
+                    TaskPlayAnim(ped, "anim@heists@ornate_bank@thermal_charge", "cover_eyes_intro", 8.0, 8.0, 1000, 36, 1, 0, 0, 0)
+                    TaskPlayAnim(ped, "anim@heists@ornate_bank@thermal_charge", "cover_eyes_loop", 8.0, 8.0, 3000, 49, 1, 0, 0, 0)
+                    Citizen.Wait(8500)
+                    ClearPedTasks(ped)
+                    DeleteObject(bomba)
+                    DeleteObject(bag)
+                    StopParticleFxLooped(effect, 0)
+                    Citizen.Wait(2000)
+                    
+                    TriggerServerEvent("drp-ud:setOngoingHeist", true)
+                end,
+                function()
+
+                end)
+            else
+                TriggerEvent('DoLongHudText', 'Not enough police on duty.', 2)
             end
-            TaskGoStraightToCoord(PlayerPedId(), 8.4783, -667.8687, 33.4497, 1.0, -1, 177.2868, 0.0)
-            Citizen.Wait(4000)
-            TriggerEvent('inventory:removeItem', 'elevatorhackingdevice', 1)
-            exports["blz-memory"]:thermiteminigame(1, 3, 3, 8,
-            function()
-                local rotx, roty, rotz = table.unpack(vec3(GetEntityRotation(PlayerPedId())))
-                local bagscene = NetworkCreateSynchronisedScene(8.4783, -667.8687, 33.4497, rotx, roty, rotz + 1.1, 2, false, false, 1065353216, 0, 1.3)
-                local bag = CreateObject(GetHashKey("hei_p_m_bag_var22_arm_s"), 8.4783, -667.8687, 33.4497,  true,  true, false)
-
-                SetEntityCollision(bag, false, true)
-                NetworkAddPedToSynchronisedScene(ped, bagscene, "anim@heists@ornate_bank@thermal_charge", "thermal_charge", 1.2, -4.0, 1, 16, 1148846080, 0)
-                NetworkAddEntityToSynchronisedScene(bag, bagscene, "anim@heists@ornate_bank@thermal_charge", "bag_thermal_charge", 4.0, -8.0, 1)
-                SetPedComponentVariation(ped, 5, 0, 0, 0)
-                NetworkStartSynchronisedScene(bagscene)
-                Citizen.Wait(1500)
-                local x, y, z = table.unpack(GetEntityCoords(ped))
-                local bomba = CreateObject(GetHashKey("hei_prop_heist_thermite"), x, y, z + 0.3, true, true, true)
-
-                SetEntityCollision(bomba, false, true)
-                AttachEntityToEntity(bomba, ped, GetPedBoneIndex(ped, 28422), 0, 0, 0, 0, 0, 200.0, true, true, false, true, 1, true)
-                Citizen.Wait(2000)
-                DeleteObject(bag)
-                SetPedComponentVariation(ped, 5, 45, 0, 0)
-                DetachEntity(bomba, 1, 1)
-                FreezeEntityPosition(bomba, true)
-                TriggerServerEvent("drp-ud:particleserver2", method, vector3(x, y, z + 0.3))
-                SetPtfxAssetNextCall("scr_ornate_heist")
-                local effect = StartParticleFxLoopedAtCoord("scr_heist_ornate_thermal_burn", x, y, z + 0.3, 0.0, 0.0, 0.0, 1.0, false, false, false, false)
-                
-                NetworkStopSynchronisedScene(bagscene)
-                TaskPlayAnim(ped, "anim@heists@ornate_bank@thermal_charge", "cover_eyes_intro", 8.0, 8.0, 1000, 36, 1, 0, 0, 0)
-                TaskPlayAnim(ped, "anim@heists@ornate_bank@thermal_charge", "cover_eyes_loop", 8.0, 8.0, 3000, 49, 1, 0, 0, 0)
-                Citizen.Wait(8500)
-                ClearPedTasks(ped)
-                DeleteObject(bomba)
-                DeleteObject(bag)
-                StopParticleFxLooped(effect, 0)
-                Citizen.Wait(2000)
-                
-                TriggerServerEvent("drp-ud:setOngoingHeist", true)
-            end,
-            function()
-
-            end)
         else
-            TriggerEvent('DoLongHudText', 'Not enough police on duty.', 2)
+            TriggerEvent('DoLongHudText', 'Already burned.', 2)
         end
     end
 end)
