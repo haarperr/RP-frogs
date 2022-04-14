5da03656df2dec97cd6c032d806678750ca6e5cd
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 05:27:38 +0200

    x

--- fx-server-data/resources/[Royal]/drp-uniondepository/client.lua
+++ fx-server-data/resources/[Royal]/drp-uniondepository/client.lua
@@ -129,9 +129,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(1)        
         count = count + 1
         if count <= 1000 then
-            Citizen.Trace(defenderSpawned)
-            Citizen.Trace(ongoingHeist)
-
+            
             TriggerServerEvent("drp-ud:checkOngoingHeist")
             count = 0
         end
