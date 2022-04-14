4ee339a43f8a0fddf9031d3f9752f932f6007e4a
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 14:51:13 +0200

    x

--- fx-server-data/resources/[Royal]/drp-vehicles/client/cl_vehiclemod.lua
+++ fx-server-data/resources/[Royal]/drp-vehicles/client/cl_vehiclemod.lua
@@ -1217,7 +1217,7 @@ cfg = {
 
 	compatibilityMode = false,					-- prevents other scripts from modifying the fuel tank health to avoid random engine failure with BVA 2.01 (Downside is it disabled explosion prevention)
 
-	randomTireBurstInterval = 45,				-- Number of minutes (statistically, not precisely) to drive above 22 mph before you get a tire puncture. 0=feature is disabled
+	randomTireBurstInterval = 120,				-- Number of minutes (statistically, not precisely) to drive above 22 mph before you get a tire puncture. 0=feature is disabled
 
 	classDamageMultiplier = {
 		[0] = 	0.8,		--	0: Compacts
