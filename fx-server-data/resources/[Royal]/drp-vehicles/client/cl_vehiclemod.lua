8aaee3055d20ba6fe380fef1b2497935c216fb16
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 15:37:36 +0200

    x

--- fx-server-data/resources/[Royal]/drp-vehicles/client/cl_vehiclemod.lua
+++ fx-server-data/resources/[Royal]/drp-vehicles/client/cl_vehiclemod.lua
@@ -196,7 +196,7 @@ end
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(2000)
+        Citizen.Wait(2500)
         stallSpamCheck = stallSpamCheck - 1
         if stallSpamCheck < 0 then
             stallSpamCheck = 0
