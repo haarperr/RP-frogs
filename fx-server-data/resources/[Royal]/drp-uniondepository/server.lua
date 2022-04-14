8cead5f0afc72219ce33e9aed3e4d6d0ae7ca881
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-14 10:17:34 +0200

    x

--- fx-server-data/resources/[Royal]/drp-uniondepository/server.lua
+++ fx-server-data/resources/[Royal]/drp-uniondepository/server.lua
@@ -4,35 +4,35 @@ AddEventHandler("drp-ud:particleserver", function(method, vec3)
     TriggerClientEvent("drp-ud:particleclient", -1, method, vec3)
 end)
 
-local whatIsThis = {}
-whatIsThis["ongoingHeist"] = false
-whatIsThis["defenderSpawned"] = false
-whatIsThis["defender2Spawned"] = false
-whatIsThis["totalThermite"] = 0
-whatIsThis["isElevatorBurned"] = false
-whatIsThis["isThermite1Burned"] = false
-whatIsThis["isThermite2Burned"] = false
-whatIsThis["isThermite3Burned"] = false
-whatIsThis["isThermite4Burned"] = false
+local s = {}
+s["ongoingHeist"] = false
+s["defenderSpawned"] = false
+s["defender2Spawned"] = false
+s["totalThermite"] = 0
+s["isElevatorBurned"] = false
+s["isThermite1Burned"] = false
+s["isThermite2Burned"] = false
+s["isThermite3Burned"] = false
+s["isThermite4Burned"] = false
 
 
 
 RegisterServerEvent("drp-ud:requestVariables")
 AddEventHandler("drp-ud:requestVariables", function()
-    Citizen.Trace("Requesting variables: " .. tostring(whatIsThis) .. "\n")
-    TriggerClientEvent("drp-ud:getVariables", -1, whatIsThis)
+    Citizen.Trace("Requesting variables: " .. tostring(s["ongoingHeist"]) .. " " .. tostring(s["defenderSpawned"]) .. " " .. tostring(s["defender2Spawned"]) .. " " .. tostring(s["totalThermite"]) .. " " .. tostring(s["isElevatorBurned"]) .. " " .. tostring(s["isThermite1Burned"]) .. " " .. tostring(s["isThermite2Burned"]) .. " " .. tostring(s["isThermite3Burned"]) .. " " .. tostring(s["isThermite4Burned"]) .. "\n")
+    TriggerClientEvent("drp-ud:getVariables", -1, s)
 end) 
 
 RegisterServerEvent("drp-ud:setVariable")
 AddEventHandler("drp-ud:setVariable", function(variable, bool)
     defenderSpawned = bool
-    Citizen.Trace("Requesting variables: " .. tostring(whatIsThis) .. "\n")
-    TriggerClientEvent("drp-ud:getVariables", -1, whatIsThis)
+    Citizen.Trace("Requesting variables: " .. tostring(s["ongoingHeist"]) .. " " .. tostring(s["defenderSpawned"]) .. " " .. tostring(s["defender2Spawned"]) .. " " .. tostring(s["totalThermite"]) .. " " .. tostring(s["isElevatorBurned"]) .. " " .. tostring(s["isThermite1Burned"]) .. " " .. tostring(s["isThermite2Burned"]) .. " " .. tostring(s["isThermite3Burned"]) .. " " .. tostring(s["isThermite4Burned"]) .. "\n")
+    TriggerClientEvent("drp-ud:getVariables", -1, s)
 end)
 
 RegisterServerEvent("drp-ud:addThermite")
 AddEventHandler("drp-ud:addThermite", function()
-    whatIsThis.totalThermite = whatIsThis.totalThermite + 1
-    Citizen.Trace("Requesting variables: " .. tostring(whatIsThis) .. "\n")
-    TriggerClientEvent("drp-ud:getVariables", -1, whatIsThis)
+    s.totalThermite = s.totalThermite + 1
+    Citizen.Trace("Requesting variables: " .. tostring(s["ongoingHeist"]) .. " " .. tostring(s["defenderSpawned"]) .. " " .. tostring(s["defender2Spawned"]) .. " " .. tostring(s["totalThermite"]) .. " " .. tostring(s["isElevatorBurned"]) .. " " .. tostring(s["isThermite1Burned"]) .. " " .. tostring(s["isThermite2Burned"]) .. " " .. tostring(s["isThermite3Burned"]) .. " " .. tostring(s["isThermite4Burned"]) .. "\n")
+    TriggerClientEvent("drp-ud:getVariables", -1, s)
 end)
