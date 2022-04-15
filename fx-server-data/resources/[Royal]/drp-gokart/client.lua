fce2cd7fb11793084ecd7be12f59f5c3d7e825a4
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-15 15:34:22 +0200

    x

--- fx-server-data/resources/[Royal]/drp-gokart/client.lua
+++ fx-server-data/resources/[Royal]/drp-gokart/client.lua
@@ -4,10 +4,6 @@ AddEventHandler("drp-gokart:getcart", function()
     local hash = GetHashKey("veto2")
     local player = GetPlayerPed(-1)
 
-    -- remove 500$
-    TriggerServerEvent("drp-gokart:removemoney")
-    
-
     local cartSpawn = vector4(5547.5283, 289.1031, 20.1434, 263.5504)
     
     RequestModel(hash)
@@ -17,15 +13,9 @@ AddEventHandler("drp-gokart:getcart", function()
 
     local cart = CreateVehicle(hash, cartSpawn.x, cartSpawn.y, cartSpawn.z, cartSpawn.w, true, true)
     SetVehicleOnGroundProperly(cart)
-    SetVehicleNumberPlateText(cart, "GoKart")
     SetVehicleHasBeenOwnedByPlayer(cart, true)
     SetVehicleNeedsToBeHotwired(cart, false)
     SetVehicleEngineOn(cart, true, true)
     SetVehicleUndriveable(cart, false)
     SetPedIntoVehicle(player, cart, -1)
-    
-    
-
-
-
 end)
--- fx-server-data/resources/[Royal]/drp-gokart/server.lua
+++
@@ -1,9 +0,0 @@
-RegisterNetEvent("drp-gokart:removemoney")
-AddEventHandler("drp.gokart:removemoney", function()
-    local player = GetPlayerPed(-1)
-    local money = GetPlayerMoney(player)
-    if money >= 500 then
-        local newMoney = money - 500
-        SetPlayerMoney(player, newMoney)
-    end
-end)
