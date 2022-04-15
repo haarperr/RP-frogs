7da59ac9c088d3b2a37b2a6d59ae25b76d6e318d
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-15 22:36:51 +0200

    x

--- fx-server-data/resources/[Vault]/hacking3/html/src/puzzle-handler.js
+++ fx-server-data/resources/[Vault]/hacking3/html/src/puzzle-handler.js
@@ -6,7 +6,7 @@ const progressBar = $('.answer-progress-bar')
 const inputElement = $('.answer-input')
 
 let puzzleTime = 10
-let puzzleAmount = 5
+let puzzleAmount = 1
 
 // handles generating puzzle and returning result
 export async function doPuzzle(){
