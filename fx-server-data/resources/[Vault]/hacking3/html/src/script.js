df188cddef4198c3d1614cf1c9556bb1bffd544e
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-15 22:33:27 +0200

    x

--- fx-server-data/resources/[Vault]/hacking3/html/src/script.js
+++ fx-server-data/resources/[Vault]/hacking3/html/src/script.js
@@ -30,7 +30,7 @@ async function start(){
     let answer
     let result = true
 
-    for (let i = 0; i < 6 && result; i++) {
+    for (let i = 0; i < 5 && result; i++) {
         [submitted, answer] = await doPuzzle()
         result = (submitted?.toLowerCase() == answer)
     }
