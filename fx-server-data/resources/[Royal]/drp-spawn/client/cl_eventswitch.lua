4c37f9b672b5c5d2628af86735bf0da0bc463125
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-15 06:40:23 +0200

    x found that in code

--- fx-server-data/resources/[Royal]/drp-spawn/client/cl_eventswitch.lua
+++ fx-server-data/resources/[Royal]/drp-spawn/client/cl_eventswitch.lua
@@ -92,7 +92,18 @@ function Login.characterSpawned()
   end
 
   SetPedMaxHealth(PlayerPedId(), 200)
-  --SetPlayerMaxArmour(PlayerId(), 100) -- This is setting players armor on relog??
+  SetPlayerMaxArmour(PlayerId(), 100) -- This is setting players armor on relog??
+
+  DisplayRadar(0)
+  SetRadarBigmapEnabled(true, false)
+  Citizen.Wait(0)
+  SetRadarBigmapEnabled(false, false)
+  DisplayRadar(0)
+  Citizen.Wait(0) 
+  sendAppEvent("hud", {
+      display = true,
+  })
+  
   Spawn.isNew = false
   exports.spawnmanager:setAutoSpawn(false)
   runGameplay()
