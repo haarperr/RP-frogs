3f19cc4330cb0f00b8b076242c13b4dbf3cadf3d
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-15 16:05:46 +0200

    x

--- fx-server-data/resources/[Royal]/drp-gokart/client.lua
+++ fx-server-data/resources/[Royal]/drp-gokart/client.lua
@@ -27,3 +27,81 @@ AddEventHandler("drp-gokart:recievekart", function()
 
     SetPedIntoVehicle(player, cart, -1)
 end)
+
+goKartEnter = false
+goKartExit = false
+
+Citizen.CreateThread(function()
+    exports["drp-polyzone"]:AddBoxZone("gokartexit", vector3(5469.13, 260.61, 20.07), 3, 3, {
+        name="gokartexit",
+        heading=0,
+        --debugPoly=true
+    })
+end)
+
+
+RegisterNetEvent('drp-polyzone:enter')
+AddEventHandler('drp-polyzone:enter', function(name)
+    if name == "gokartexit" then
+        goKartExit = true     
+        gokartexit()
+        
+    end
+end)
+
+RegisterNetEvent('drp-polyzone:exit')
+AddEventHandler('drp-polyzone:exit', function(name)
+    if name == "gokartexit" then
+        goKartExit = false
+        exports['drp-textui']:hideInteraction()
+    end
+end)
+
+function gokartexit()
+	Citizen.CreateThread(function()
+        while goKartExit do
+            Citizen.Wait(5)
+			if IsControlJustReleased(0, 38) then
+                SetEntityCoords(GetPlayerPed(-1), -253.76, -1992.06, 30.15)
+			end
+		end
+	end)
+end
+
+
+Citizen.CreateThread(function()
+    exports["drp-polyzone"]:AddBoxZone("gokartenter", vector3(vector3(-253.76, -1992.06, 30.15), 3, 3, {
+        name="gokartenter",
+        heading=75,
+        --debugPoly=true
+    })
+end)
+
+
+RegisterNetEvent('drp-polyzone:enter')
+AddEventHandler('drp-polyzone:enter', function(name)
+    if name == "gokartenter" then
+        goKartEnter = true     
+        gokartenter()
+        
+    end
+end)
+
+RegisterNetEvent('drp-polyzone:exit')
+AddEventHandler('drp-polyzone:exit', function(name)
+    if name == "gokartenter" then
+        goKartEnter = false
+        exports['drp-textui']:hideInteraction()
+    end
+end)
+
+function gokartenter()
+    Citizen.CreateThread(function()
+        while goKartEnter do
+            Citizen.Wait(5)
+            if IsControlJustReleased(0, 38) then
+                SetEntityCoords(GetPlayerPed(-1), 5472.5215, 254.6226, 19.0711)
+            end
+        end
+    end)
+end
