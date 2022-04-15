73eb71e067a0f4712e598a66ec677eb8e54ec0df
Greenlio <greenliobusiness@gmail.com>
2022-04-14 23:08:26 -0500

    create foundation for shenanigans

---
+++ fx-server-data/resources/[WIP]/[casino]/rpfrogs-blackjack/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game "gta5"
+
+name 'rpfrogs-blackjack'
+description 'gamba'
+
+shared_scripts {
+	'coords.lua',
+	'@drp-lib/import.lua'
+}
+
+client_script 'timerbars.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+pizza_topping "pineapple"
