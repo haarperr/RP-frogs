b436f2964ddb58bbaa1814c5492415c06fc9133f
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-16 09:20:07 +0200

    x

--- fx-server-data/resources/[Royal]/drp-errorlog/server/sv_errorlog.lua
+++ fx-server-data/resources/[Royal]/drp-errorlog/server/sv_errorlog.lua
@@ -17,7 +17,7 @@ function sendToDiscord(name, args, color)
               },
           }
       }
-    PerformHttpRequest('https://discord.com/api/webhooks/956807237010067506/RbZsiM3_qjmXIDN2BJdAnvPdY3xzWEHEtdr6vx4GzgIClMRMox3yL59Ao9eyZT6d21iW"}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest('https://discord.com/api/webhooks/956807237010067506/RbZsiM3_qjmXIDN2BJdAnvPdY3xzWEHEtdr6vx4GzgIClMRMox3yL59Ao9eyZT6d21iW", { ['Content-Type'] = 'application/json' })
 end
 
 -- it must be saving into a file with io.open("test.lua", "r")
