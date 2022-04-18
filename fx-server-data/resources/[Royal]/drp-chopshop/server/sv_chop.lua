f6c2113e69a4b752c0ef6e549899c47d8fe2244c
Tamino Seiler <49478749+Nono-04@users.noreply.github.com>
2022-04-18 07:40:40 +0200

    x

--- fx-server-data/resources/[Royal]/drp-chopshop/server/sv_chop.lua
+++ fx-server-data/resources/[Royal]/drp-chopshop/server/sv_chop.lua
@@ -4,14 +4,14 @@ AddEventHandler("drp-chopshop:vehicle_loot", function(string)
     local user = exports["drp-base"]:getModule("Player"):GetUser(src)
     local Reward= math.random(1500, 2000)
     if string == "tyre" then
-        TriggerClientEvent("player:receiveItem", src, "recyclablematerial", math.random(4, 7), false)
+        TriggerClientEvent("player:receiveItem", src, "recyclablematerial", math.random(5, 9), false)
     elseif string == "door" then
-        TriggerClientEvent("player:receiveItem", src, "recyclablematerial", math.random(5, 8), false)
+        TriggerClientEvent("player:receiveItem", src, "recyclablematerial", math.random(7, 11), false)
     elseif string == "remains" then
         local roll = math.random(1, 2)
         if roll == 1 then
             TriggerClientEvent('DoLongHudText', src, 'Well this one was bad wasnt it ...', 2)
-            TriggerClientEvent('player:receiveItem', src, 'recyclablematerial', math.random(10, 20))
+            TriggerClientEvent('player:receiveItem', src, 'recyclablematerial', math.random(12, 20))
         elseif roll == 2 then
             TriggerClientEvent('DoLongHudText', src, 'Well this was good wasnt it ...', 1)
             TriggerClientEvent('player:receiveItem', src, 'recyclablematerial', math.random(20, 30))
